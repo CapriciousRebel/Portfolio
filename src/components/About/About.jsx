f03252d1a9bfddc1a8ad26fc38b9a3f0f01bbd5a
CapriciousRebel <code.capriciousrebel@gmail.com>
2021-05-25 07:27:14 +0530

    add about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -56,7 +56,7 @@ const About = () => {
                       className="cta-btn cta-btn--resume"
                       href={resume}
                     >
-                      Resume
+                      Résumé
                     </a>
                   </span>
                 )}
--- src/mock/data.js
+++ src/mock/data.js
@@ -18,22 +18,26 @@ export const heroData = {
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  paragraphOne:
+    "I'm a sophomore pursuing Electronics and Communications Engineering from IIT Roorkee.",
+  paragraphTwo:
+    'My interests lie in web development and game development. For me, the joy of software development lies in seeing my work impact people’s lives, like seeing my friends enjoy the games I made or people benefit from the webapps I make...',
+  paragraphThree:
+    "I'm a developer at SDSLabs, a student run coding group at IIT Roorkee. Most of my technical learning has progressed while being a member of SDSLabs.",
+  resume: 'https://drive.google.com/file/d/1c3PFLj3Rz71sP-54cSjwU6v6dX86RH_z/view', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'covidvaccinetracker.png',
+    title: 'Covid Vaccine Tracker for India',
+    info: 'A user friendly webapp written using ReactJS, deployed on github pages that keeps checking for the availability of a vaccination slot in the given pincode and then notifies the user with a sound.',
+    info2:
+      'Includes filters like type of vaccine, price of vaccine, allowed ages for the users to get notified only for the slot that suits them.',
+    url: 'https://capriciousrebel.github.io/CovidVaccineTracker/#/',
+    repo: 'https://github.com/CapriciousRebel/CovidVaccineTracker', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
